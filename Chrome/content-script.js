47cc342dc30f7ee94f4167b38637b82170114ba5
Aaron Ogle <geekgonecrazy@users.noreply.github.com>
2017-05-04 14:16:55 -0500

    only set cookie and connect if RocketChat object available

--- Chrome/content-script.js
+++ Chrome/content-script.js
@@ -1,23 +1,25 @@
 ﻿// FORKED FROM https://github.com/muaz-khan/WebRTC-Experiment/tree/master/Chrome-Extensions/desktopCapture
 
-document.cookie = "rocketchatscreenshare=chrome";
+if (typeof window.RocketChat !== 'undefined') {
+	document.cookie = "rocketchatscreenshare=chrome";
 
 
-// this port connects with background script
-var port = chrome.runtime.connect();
+	// this port connects with background script
+	var port = chrome.runtime.connect();
 
 
-// get message from background script and forward to the webpage
-port.onMessage.addListener(function (message) {
-	window.postMessage(message, '*');
-});
+	// get message from background script and forward to the webpage
+	port.onMessage.addListener(function (message) {
+		window.postMessage(message, '*');
+	});
 
 
-window.addEventListener('message', function (event) {
-	// if invalid source or invalid message
-	if (event.source !== window || event.data !== 'get-sourceId')
-		return;
+	window.addEventListener('message', function (event) {
+		// if invalid source or invalid message
+		if (event.source !== window || event.data !== 'get-sourceId')
+			return;
 
-	// forward message to background script
-	port.postMessage(event.data);
-});
+		// forward message to background script
+		port.postMessage(event.data);
+	});
+}
